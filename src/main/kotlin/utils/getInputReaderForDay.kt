eb63b97f2a52db5c19e212f194f13359c533cc66
Joozd <joozd@ius.org>
2020-11-04 09:48:34 +0100

    deleted

--- src/main/kotlin/utils/getInputReaderForDay.kt
+++
@@ -1,9 +0,0 @@
-package utils
-
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
-import java.io.InputStreamReader
-
-fun getInputReaderForDay(day: Int): BufferedReader =
-    File("inputs\\day$day.txt").inputStream().bufferedReader()
