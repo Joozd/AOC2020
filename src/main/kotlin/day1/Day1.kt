edd509ee3b799225f987cbc81c66d313b875dd63
Joozd <joozd@ius.org>
2020-12-01 09:23:18 +0100

    Day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,14 +1,27 @@
 package day1
 
 import Solution
+import utils.PairMaker
 
 class Day1(day: Int): Solution(day) {
-    // val extraInput = getExtraInputLinesForDay(dayNumber, "a")
+    private val entries = inputLines.map{it.toInt()}
 
-    override val first = one()
+
+    override val first: String
+        get() = PairMaker(entries).firstOrNull{ it.first + it.second == 2020}?.let{ it.first * it.second}.toString()
     override val second = two()
 
-    private fun one() = "Answer to question 1"
+    // private fun one(): String =
 
-    private fun two() = "Answer to question 2"
+
+
+    private fun two(): String{
+        val pairs = PairMaker(entries)
+        entries.forEach { third ->
+            pairs.firstOrNull { it.first + it.second + third == 2020 }?.let{
+                return("${it.first} * ${it.second} * $third = ${it.first * it.second * third} (sum is ${it.first + it.second + third})")
+            }
+        }
+        return "none found"
+    }
 }
---
+++ src/main/kotlin/utils/PairMaker.kt
@@ -0,0 +1,26 @@
+package utils
+
+/**
+ * Iterates over al pairs in [list]
+ * eg. listOf(1,2,3) will become [(1, 2), (1, 3), (2, 3)]
+ */
+class PairMaker<T>(val list: List<T>): Iterable<Pair<T,T>> {
+    override fun iterator(): Iterator<Pair<T,T>> = object: Iterator<Pair<T,T>>{
+        private var firstIndex: Int = 0
+        private var secondIndex: Int = 1
+        override fun hasNext(): Boolean =
+            firstIndex != list.indices.last
+
+        override fun next(): Pair<T,T> = (list[firstIndex] to list[secondIndex]).also{
+            setNextIndex()
+        }
+
+        private fun setNextIndex(){
+            if (secondIndex == list.indices.last) {
+                firstIndex++
+                secondIndex = firstIndex + 1
+            }
+            else secondIndex++
+        }
+    }
+}
