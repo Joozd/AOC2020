beeed359c226aebc663277d2809f97a25ef4c814
Joozd <joozd@ius.org>
2020-12-05 12:52:59 +0100

    day 5 shorter & faster

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -3,16 +3,17 @@ package day5
 import Solution
 
 class Day5(day: Int): Solution(day) {
-    val seatIds by lazy {inputLines.map {it.seatId()}}
+    //lazy function for my private timing needs.
+    private val seatIds by lazy {
+        inputLines.map {l -> l.map{(it.toInt()%7%2).toString()}.joinToString("").toInt(2)}
+    }
 
+    // get the highest seatID
     override val first: String
         get() = seatIds.maxOrNull().toString()
+
+    // get the first seatId that has an empty one after it, and a filled one after that. The one after it is the answer.
     override val second: String
-        get() = (0.."1111111111".toInt(2)).filter {it !in seatIds}.let{missing ->
-            missing.filter { it-1 !in missing && it+1 !in missing }
-        }.toString()
+        get() = (seatIds.first { it+1 !in seatIds && it+2 in seatIds }+1).toString()
 
-
-
-    private fun String.seatId() = replace("[BR]".toRegex(), "1").replace("[FL]".toRegex(), "0").toInt(2)
 }
