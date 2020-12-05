e1f8d147481086a89d5de1f55776de1aa934c78f
Joozd <joozd@ius.org>
2020-12-05 08:44:13 +0100

    day 5

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -3,18 +3,16 @@ package day5
 import Solution
 
 class Day5(day: Int): Solution(day) {
-    // val extraInput = getExtraInputLinesForDay(dayNumber, "a")
+    val seatIds by lazy {inputLines.map {it.seatId()}}
 
     override val first: String
-        get() = one()
+        get() = seatIds.maxOrNull().toString()
     override val second: String
-        get() = two()
+        get() = (0.."1111111111".toInt(2)).filter {it !in seatIds}.let{missing ->
+            missing.filter { it-1 !in missing && it+1 !in missing }
+        }.toString()
 
-    private fun one(): String {
-        return "Answer to question 1"
-    }
 
-    private fun two(): String {
-        return "Answer to question 2"
-    }
+
+    private fun String.seatId() = replace("[BR]".toRegex(), "1").replace("[FL]".toRegex(), "0").toInt(2)
 }
