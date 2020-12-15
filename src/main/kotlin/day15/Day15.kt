37f8f2e61e7bb2c575cda5177f84a3bb2b06e497
Joozd <joozd@ius.org>
2020-12-15 08:50:58 +0100

    day15

---
+++ src/main/kotlin/day15/Day15.kt
@@ -0,0 +1,27 @@
+package day15
+
+import Solution // Contains functions for reading input file and timing how long it takes to get the answers
+
+class Day15(day: Int): Solution(day) {
+    private val initialCalls by lazy {
+        inputLines.first().split(',').mapIndexed { index, s -> s.toInt() to index + 1 }.toMap()
+    }
+
+    override val first: String
+        get() = calculate(2020).toString()
+    override val second: String
+        get() = calculate(30000000).toString()
+
+    private fun calculate(target: Int): Int {
+        val database = initialCalls.toMutableMap()
+        var counter = initialCalls.values.maxOrNull()!! + 1
+        var next = 0
+        while (counter < target){
+            val nextNext = counter - (database[next] ?: counter)
+            //println("$counter: $next ($nextNext steps repeat)")
+            database[next] = counter++
+            next = nextNext
+        }
+        return next
+    }
+}
