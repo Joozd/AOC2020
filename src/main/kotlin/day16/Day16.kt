b0095402d1881c5ba16e688e628e3fdaf8249e72
Joozd <joozd@ius.org>
2020-12-16 14:00:32 +0100

    day16

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -8,9 +8,7 @@ import utils.extensions.splitToWords
  * Least amount of lines for extra unreadibility!
  */
 class Day16(day: Int): Solution(day) {
-    //rules is a list of all rules per line
     private val rules by lazy { inputGroups.first().map { it.splitToWords().takeLast(3).mapNotNull { w -> w.takeIf { '-' in it }?.split('-')?.let { r -> r.first().toInt()..r.last().toInt() }}}}
-
     private val ticketLines by lazy { inputGroups.last().drop(1).map { it.split(',').map { it.toInt() }}}
 
     override val first: String
@@ -21,15 +19,9 @@ class Day16(day: Int): Solution(day) {
     private fun one(): Int = rules.flatten().let { rr -> ticketLines.flatten().filter { v -> rr.none { r -> v in r }}.sum() }
 
     private fun two(): Long {
-        //map of fields (ticket positions) to all matching rules for that value
         val tickets = ticketLines.map { values -> values.mapIndexed { index, i -> index to rules.mapIndexedNotNull { rule, ranges -> rule.takeIf { ranges.any { i in it }}} }.toMap() }.filter{it.values.none{ mf -> mf.isEmpty()}}
-
-        // ticket positions to all possible rules for that position
         val possibilities = rules.indices.map{ rule-> rule to tickets.map{it.filterValues { rules -> rule in rules }.keys}.multiIntersect()}
-
-        val rules = HashMap<Int, Int>() // rule number (line number) to ticket position
-
-        // i know this is ugly but it works. Leave me alone.
+        val rules = HashMap<Int, Int>()
         while (rules.keys.size != possibilities.size){ possibilities.first { it.second.filter{it !in rules.keys}.size == 1 }.let { next -> rules[next.second.first{it !in rules.keys}] = next.first } }
         inputGroups[1].drop(1).first().split(',').map{it.toInt()}.let { myTicket -> return (0 until 6).fold(1L) { acc, i -> acc * myTicket[rules[i]!!] } }
     }
