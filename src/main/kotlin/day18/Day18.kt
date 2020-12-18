a97c69663e1d520aa50a481c9c0c2d2b76104b05
Joozd <joozd@ius.org>
2020-12-18 15:32:54 +0100

    day 18

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -3,37 +3,11 @@ package day18
 import Solution // Contains functions for reading input file and timing how long it takes to get the answers
 
 class Day18(day: Int): Solution(day) {
-    override val inputLines = test11
-    val problems by lazy { inputLines.map {MathProblem.of(it)} }
+    private val problems by lazy { inputLines.map {MathProblem.of(it)} }
 
     override val first: String
-        get() = one().toString()
+        get() = problems.map{it.value}.sum().toString()
     override val second: String
-        get() = two()
+        get() = problems.map{it.value2}.sum().toString().toString()
 
-    // 1208361047 too low
-    // 98721031659607 too high
-    // 98202646080000 too low
-    private fun one(): Long {
-        problems.forEach{
-            // println("${it.value.toString().padEnd(15)}:  ${it.string}")
-            println(it.value)
-        }
-        return problems.map{it.value}.sum()
-    }
-
-    private fun two(): String {
-        return "Answer to question 2"
-    }
-
-    companion object{
-        val test1 = ("1 + 2 * 3 + 4 * 5 + 6").lines()
-        val test2 = "1 + (2 * 3) + (4 * (5 + 6))".lines()
-        val test3 = "2 * 3 + (4 * 5)".lines()
-        val test4 = "5 + (8 * 3 + 9 + 3 * 4 * 3)".lines()
-        val test5 = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".lines()
-        val test6 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".lines()
-        val test10 = "4 + (7 + 7 * 6 + 3 * 9) * (111104) + 3".lines()
-        val test11 = "4 + (7 + 7 * 6 + 3 * 9) * ((4 + 2 * 4 + 4 + 9 + 7) * 5 * 9 * (3 + 7 + 4 * 4) + (4 * 8 * 7)) + 3".lines()
-    }
 }
--- src/main/kotlin/day18/MathProblem.kt
+++ src/main/kotlin/day18/MathProblem.kt
@@ -5,7 +5,7 @@ import utils.extensions.splitToWords
 import java.util.*
 import kotlin.collections.ArrayList
 
-class MathProblem(val subProblems: List<MathProblem>? = null, val operators: List<Char>? = null, val singleValue: Long? = null, val string: String? = null) {
+class MathProblem(private val subProblems: List<MathProblem>? = null, private val operators: List<Char>? = null, val singleValue: Long? = null, val string: String? = null) {
     init{
         if (subProblems != null) require (subProblems.isNotEmpty() && operators?.size == subProblems.size -1)
     }
@@ -14,13 +14,26 @@ class MathProblem(val subProblems: List<MathProblem>? = null, val operators: Lis
             when (operators!![index]) {
                 MULTIPLY -> acc * mp.value
                 ADD -> acc + mp.value
-                else -> error ("ONLY ADD OR MULTiPLY")
+                else -> error ("ONLY ADD OR MULTiPLY (got ${operators[index]})")
             }
         }
     }
 
+    private fun calculateValue2(): Long {
+        val ops = LinkedList(operators!!)
+        val values = LinkedList(subProblems!!)
+        while (ADD in ops){
+            val index = ops.indexOf(ADD)
+            values[index] = MathProblem(singleValue = (values[index].value2 + values[index+1].value2))
+            values.removeAt(index+1)
+            ops.removeAt(index)
+        }
+        return values.fold(1L) { acc, mp -> acc * mp.value2}
+    }
+
     val value: Long = singleValue ?: calculatedValue ?: error("NO SINGLE OR CALCULATED VALUE")
 
+    val value2: Long = singleValue ?: calculateValue2()
 
     companion object{
         const val MULTIPLY = '*'
@@ -30,19 +43,24 @@ class MathProblem(val subProblems: List<MathProblem>? = null, val operators: Lis
 
         private fun recursiveOf(s: String): Pair<MathProblem, String> {
             val ww = LinkedList(s.splitToWords())
-            if (ww.size == 1) return MathProblem(singleValue = ww.first().getDigits().toLong()) to ""
+            if (ww.size == 1) return (MathProblem(singleValue = ww.first().getDigits().toLong()) to "")
             val subProblems = ArrayList<MathProblem>()
             val operators = ArrayList<Char>()
             while (ww.isNotEmpty()){
                 if(ww.first().endsWith(')') || ww.size == 1) {
+                    val extraClosingBrackets = ww.first().filter{it == ')'}.drop(1).takeIf { it.isNotEmpty() }?.let {"$it "}?: ""
                     subProblems += MathProblem(singleValue = ww.removeFirst().getDigits().toLong())
-                    return (MathProblem(subProblems, operators, string = s) to ww.joinToString(" "))
+                    return MathProblem(subProblems, operators, string = s) to extraClosingBrackets + ww.joinToString(" ")
                 }
                 if (ww.first().startsWith('(')){
                     recursiveOf(ww.joinToString(" ").drop(1)).let{
                         subProblems.add(it.first)
                         ww.clear()
                         ww.addAll(it.second.splitToWords())
+                        if(ww.first().firstOrNull() == ')'){
+                            val extraClosingBrackets = ww.removeFirst().drop(1).takeIf { it.isNotEmpty() }?.let {"$it "}?: ""
+                            return MathProblem(subProblems, operators, string = s) to extraClosingBrackets + ww.joinToString(" ")
+                        }
                         ww.removeFirstOrNull()?.firstOrNull()?.let { op -> operators.add(op) }
                     }
                 }
@@ -51,7 +69,7 @@ class MathProblem(val subProblems: List<MathProblem>? = null, val operators: Lis
                     operators.add(ww.removeFirst().first())
                 }
             }
-            return MathProblem(subProblems, operators, string = s) to ""
+            return (MathProblem(subProblems, operators, string = s) to "")
         }
     }
 }
